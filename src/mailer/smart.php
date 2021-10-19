099b6155701c4a6f7975b5de18a0e9d28a5c49c2
Deadduck89 <deadduck89@yandex.ru>
2021-10-19 10:59:44 +0300

    SMTP credentials deleted

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -13,13 +13,13 @@ $mail->CharSet = 'utf-8';
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'deadduck89@gmail.com';                 // Наш логин
-$mail->Password = 'dxmqgzjuegsdfbjq';                           // Наш пароль от ящика
+$mail->Username = '';                 // Наш логин
+$mail->Password = '';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
 $mail->setFrom('Pulse', 'Pulse');   // От кого письмо 
-$mail->addAddress('gocik18950@wii999.com');     // Add a recipient
+$mail->addAddress('');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
