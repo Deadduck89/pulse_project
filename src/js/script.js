d75eda401d4785580aa5e0e6f63b0a8a94f1b94e
Deadduck89 <deadduck89@yandex.ru>
2021-10-19 15:00:35 +0300

    Chevron for scroll up

--- src/js/script.js
+++ src/js/script.js
@@ -107,5 +107,16 @@ $(document).ready(function(){
         $('form').trigger('reset');
       });
       return false;
-    })
+    });
+
+    //Smooth scroll
+
+    $(window).scroll(function(){
+      if ($(this).scrollTop() > 1600) {
+        $('.pageup').fadeIn();
+      } else {
+        $('.pageup').fadeOut();
+      }
+    });
+    new WOW().init();
 });
